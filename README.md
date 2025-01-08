e6b4e8d3eb048af841a7ea978b5ff669038fcfc9
Tine <74303770+kristinejabson@users.noreply.github.com>
2025-01-08 21:43:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# texttospeech
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/texttospeech/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.texttospeech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
